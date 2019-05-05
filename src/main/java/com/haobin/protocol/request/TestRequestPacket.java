32c889cea4dc74595c79ceddd702dd36662c7549
haobin <815754194@qq.com>
2019-05-05 11:12:45 +0800

    测试packet修改

---
+++ src/main/java/com/haobin/protocol/request/TestRequestPacket.java
@@ -0,0 +1,15 @@
+package com.haobin.protocol.request;
+
+import com.haobin.protocol.Packet;
+
+public class TestRequestPacket extends Packet {
+
+
+
+
+
+    @Override
+    public Byte getCommand() {
+        return null;
+    }
+}
